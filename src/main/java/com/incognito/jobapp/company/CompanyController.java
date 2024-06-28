3331646735374a3b08fd989c9367e08185ba27d4
Nawodya Jayalath <nawodyaonline@gmail.com>
2024-06-28 19:27:17 +0530

    add: Update company

--- src/main/java/com/incognito/jobapp/company/CompanyController.java
+++ src/main/java/com/incognito/jobapp/company/CompanyController.java
@@ -1,8 +1,8 @@
 package com.incognito.jobapp.company;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +18,12 @@ public class CompanyController {
     @GetMapping
     public List<Company> getAllCompanies() {
         return companyService.getAllCompanies();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> update(@RequestBody Company company, @RequestParam Long id) {
+        companyService.updateCompany(company, id);
+        return new ResponseEntity<>("Update successfully!", HttpStatus.OK);
     }
 
 }
